b2f4528460af8fbf8189df15031020d21d92aee5
Mikey <michael.schubert@kearsarge.org>
2017-11-28 14:18:41 -0500

    More movement stuff

--- Player.py
+++ Player.py
@@ -1,9 +1,14 @@
 import sys, pygame, math
 
-class Player:
-    def __init__(self, rImage, lImage):
-        self.rImage = pygame.load.image(rImage)
-        self.lImage = pygame.load.image(lImage)
+class Player():
+    def __init__(self, pos):
+        self.rImage = pygame.image.load("Images\Player\StickFigure\LPlayerPlaceHolder.png")
+        self.rImage = pygame.transform.scale(self.rImage,[80,240])
+        self.lImage = pygame.image.load("Images\Player\StickFigure\RPlayerPlaceHolder.png")
+        self.lImage = pygame.transform.scale(self.lImage,[80,240])
+        self.facing = "right"
+        self.image = self.rImage
+        self.rect = self.image.get_rect(center = pos)
         
         #Stats
         self.maxHealth = 100
@@ -11,6 +16,7 @@ class Player:
         self.maxCorruption = 100
         self.health = 100
         self.mana = 100
+        self.speed = [x,y] = [4,0]
         #Corruption and armor
         self.corruption = 100
         self.corruptionResistance = 0
@@ -23,8 +29,32 @@ class Player:
         #Damage
         self.damage = 2
         self.lifesteal = 0
+        self.attackSpeed = 1.0
         self.gold = 0
         #20 total inventory slots
-        self.inventory[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.inventory = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        #Other things that are not stat related
         
         
+        
+    #def move(self, speed):
+        #self.rect.move(speed)
+        
+    def go(self, movement):
+        if movement == "right":
+            #move(self.speed)
+            self.rect.move(self.speed)
+            print "Moved Right"
+        elif movement == "left":
+            #move(self.speed[0]*-1)
+            self.rect.move(self.speed)
+            print "Moved Left"
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- main.py
+++ main.py
@@ -11,7 +11,44 @@ size = width, height
 screen = pygame.display.set_mode(size)
 bgColor = r,g,b = 0, 0, 0
 
-
+player = Player([width/2, height-150])
 
 while True:
-    None
+    
+    #Handles Key Presses
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+                #Detects keys presses
+        if event.type == pygame.KEYDOWN:
+            #Sets y direction
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                None
+                #player.go("up")
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                None
+                #player.go("down")
+            #Sets x direction
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.go("right")
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.go("left")
+        #Detects Key Releases
+        if event.type == pygame.KEYUP:
+            #Stops y
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                None
+                #player.go("stop up")
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                None
+                #player.go("stop down")
+            #Stops x
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.go("stop right")
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.go("stop left")
+    
+    screen.fill(bgColor)
+    #Blit other things here
+    screen.blit(player.rImage, player.rect)
+    pygame.display.flip()
+    clock.tick(60)
