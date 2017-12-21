82ff17962af6ab10ee458ea0291bf3ee2676c2f8
Mikey <michael.schubert@kearsarge.org>
2017-12-21 13:49:34 -0500

    Worked on Item

--- Item.py
+++ Item.py
@@ -2,32 +2,35 @@ import sys, pygame, math
 
 
 class Item():
-    def __init__(self, name = "Unnamed", image = "Images/Other/Items/NoImage", description = "This is a description"):
+    def __init__(self, name = "Unnamed", image = "Images/Other/Items/NoImage", description = "This is a description", visable = False):
         self.name = name
         self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
-        
+        self.visable = visable
         #Stat Changes
         self.statList(
-            maxHealth = 0,#1
-            maxMana = 0,#2
-            maxCorruption = 0,#3
-            health = 0,#4
-            mana = 0,#5
-            speed = [0,0],#6
-            maxSpeed = [0,0],#7
+            maxHealth = 0,#0
+            maxMana = 0,#1
+            maxCorruption = 0,#2
+            health = 0,#3
+            mana = 0,#4
+            speed = [0,0],#5
+            maxSpeed = [0,0],#6
         #Corruption and armor
-            corruption = 0,#8
-            corruptionResistance = 0,#9
-            armor = 0,#10
+            corruption = 0,#7
+            corruptionResistance = 0,#8
+            armor = 0,#9
         #Xp vars
-            lvl = 1,#11
-            xp = 0,#12
-            xpMult = 0,#13
+            lvl = 1,#10
+            xp = 0,#11
+            xpMult = 0,#12
         #Damage
-            damage = 0,#14
-            lifesteal = 0,#15
-            attackSpeed = 0,#16
-            cost = 0) #17
+            damage = 0,#13
+            lifesteal = 0,#14
+            attackSpeed = 0,#15
+            cost = 0, #16
+        #Other
+            duration = 0) #17
 
-    def spawnRandom(self, type = "potion" ): None
+    def randomizeStats():
+        for stat in statList:
