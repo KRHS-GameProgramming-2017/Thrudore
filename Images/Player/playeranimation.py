a86000338522f155f4df682a8df40b9c2b3d1928
madison <madison.neal@kearsrage.org>
2017-12-21 13:48:41 -0500

    player animation fixed and scene 1 added

--- Images/Player/playeranimation.py
+++ Images/Player/playeranimation.py
@@ -1,25 +1,29 @@
 import sys, pygame, math
-from main import *
-from Player import *
+
 
 
 class PlayerAnimation():
     def __init__(self, pos):
         self.rImage = [p.i.land("path/player"),
-              [p.i.land("path/playeranimation1"),
-              [p.i.land("path/playeranimation2"),
-              [p.i.land("path/playeranimation1"),
-              [p.i.land("path/playeranimation3")]
+              [pygame.image.load("path/playeranimation1")],
+              [pygame.image.load("path/playeranimation2")],
+              [pygame.image.load("path/playeranimation1")],
+              [pygame.image.load("path/playeranimation3")]]
 
-    self.frame = 0
-    self.maxframe = len(self.rImage)-1)
-    self.image = self.rImage[self.frame]
-    self.AnimationTimer = 0
-    self.AnimationTimerMax = 30/10
+        
+        self.frame = 0
+        self.maxframe = len((self.rImage)-1)
+        self.image = self.rImage[self.frame]
+        self.AnimationTimer = 0
+        self.AnimationTimerMax = 60/10
+        
+    def animate(self):
         if AnimationTimer < AnimationTimerMax:
             AnimationTimer += 1
-        else
+        else:
             AnimationTimer = 0
-            if self.frame < self.maxframe
+            if self.frame < self.maxframe:
                 self.frame += 1
-        self.image = self.rImage[self.frame]
+            else:
+                self.frame = 0
+            self.image = self.rImage[self.frame]
