45ae281fa53b8dc864f7d6a334db806e0fb9f1ca
Mikey <michael.schubert@kearsarge.org>
2018-02-19 12:44:35 -0500

    Name change

---
+++ InventoryManager.py
@@ -0,0 +1,34 @@
+import sys, pygame, math
+
+class InventoryManager():
+    def __init__():
+        
+        self.inventorySlots[
+            "slot0" = None,
+            "slot1" = None,
+            "slot2" = None,
+            "slot3" = None,
+            "slot4" = None,
+            "slot5" = None,
+            "slot6" = None,
+            "slot7" = None,
+            "slot8" = None,
+            "slot9" = None,
+            "slot10" = None,
+            "slot11" = None,
+            "slot12" = None,
+            "slot13" = None,
+            "slot14" = None,
+            "slot15" = None,
+            "slot16" = None,
+            "slot17" = None,
+            "slot18" = None,
+            "slot19" = None,
+            "slot20" = None,
+            "slot21" = None,
+            "slot22" = None,
+            "slot23" = None
+            ]
+        
+    def putInInventory(item):
+        
